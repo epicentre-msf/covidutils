23e0b6649d7c2c27fc38820bab304c1710473d94
Paul Campbell <pacampbell91@gmail.com>
2021-01-06 17:05:01 +0000

    add get jhu data function

---
+++ R/get_owid_jhcsse.R
@@ -0,0 +1,25 @@
+
+#' Import JHU CSSE data from Our World in Data's github
+#'
+#' Download URL: https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/full_data.csv
+#'
+#' @param url raw OWID github URL for JHU CSV
+#' @return a tibble dataframe
+#' @importFrom magrittr %>%
+#' @export
+get_owid_jhcsse <- function(url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/full_data.csv") {
+  df_raw <- readr::read_csv(url, guess_max = 60000)
+  df_raw %>%
+    dplyr::filter(!location %in% c("International", "World")) %>%
+    dplyr::mutate(
+      iso_a3 = countrycode::countrycode(location, "country.name", "iso3c"),
+      iso_a3 = dplyr::case_when(location == "Timor" ~ "TLS", TRUE ~ iso_a3),
+      continent = countrycode::countrycode(iso_a3, origin = "iso3c", destination = "continent"),
+      region = countrycode::countrycode(iso_a3, origin = "iso3c", destination = "region23"),
+      continent = dplyr::case_when(location == "Kosovo" ~ "Europe", TRUE ~ continent),
+      region = dplyr::case_when(location == "Kosovo" ~ "Southern Europe", TRUE ~ region),
+      iso_a3 = dplyr::case_when(location == "Kosovo" ~ "XKX", TRUE ~ iso_a3)
+    ) %>%
+    dplyr::select(date, continent, region, country = location, iso_a3, cases = new_cases, deaths = new_deaths) %>%
+    dplyr::mutate(dplyr::across(cases:deaths, ~ ifelse(.x < 0, 0L, .x)))
+}
